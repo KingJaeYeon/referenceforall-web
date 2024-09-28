b3d607a999c09bab034ceb9d2dd221b4974b8bde
pjy <wodus331@gmail.com>
2024-09-28 22:20:11 +0900

    add: font pretendard 적용

---
+++ src/app/fonts.ts
@@ -0,0 +1,15 @@
+import localFont from "next/font/local";
+import { Noto_Sans_KR } from "next/font/google";
+
+export const pretendard = localFont({
+  src: "./fonts/PretendardVariable.woff2",
+  display: "swap",
+  variable: "--font-pretendard", // css 변수로 등록하기 위함
+});
+
+export const notoSansKR = Noto_Sans_KR({
+  weight: ["400", "700"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+});
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,9 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import React from "react";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { notoSansKR, pretendard } from "@/app/fonts";
+import Header from "@/components/Header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,9 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${pretendard.variable} ${notoSansKR.className} bg-background text-foreground`}
       >
         <Toaster />
+        <Header />
         {children}
       </body>
     </html>
