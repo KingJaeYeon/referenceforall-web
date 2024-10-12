8195013769bed47fa35a7d945c8cec5c0c4d3105
pjy <wodus331@gmail.com>
2024-10-12 20:55:38 +0900

    onmount motion

---
+++ src/app/[locale]/template.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { motion } from "framer-motion";
+import React from "react";
+import { usePathname } from "@/components/navigation";
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  return (
+    <motion.div
+      key={pathname}
+      initial={{ y: 20, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ type: "spring", stiffness: 300, damping: 20 }}
+    >
+      {children}
+    </motion.div>
+  );
+}
---
+++ src/config/index.ts
@@ -0,0 +1,3 @@
+export const locales = ["ko", "en", "jp", "cn"];
+
+export const baseURL = process.env.NEXT_PUBLIC_SITE_URL + "/api";
