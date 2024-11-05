56c51eae78014c44e6a247560a42f134f2ae91d5
pjy <wodus331@gmail.com>
2024-11-05 10:32:30 +0900

    bug

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  i18n: {
+    locales: ['en', 'ko', 'ja','cn'],
+    defaultLocale: 'ko',
+  },
+};
 
 export default withNextIntl(nextConfig);
