08f9e522b30ced1dfa1d51a415e8f2c3ae612728
pjy <wodus331@gmail.com>
2024-11-16 22:07:20 +0900

    list page tabs

--- src/app/[locale]/(auth)/my/lists/layout.tsx
+++ src/app/[locale]/(auth)/my/lists/layout.tsx
@@ -2,45 +2,16 @@ import React from "react";
 import Main from "@/components/Layout/Main";
 import Contents from "@/components/Layout/Contents";
 import Row from "@/components/Layout/Row";
-import Col from "@/components/Layout/Col";
-import Text from "@/components/Layout/Text";
-import ScrollTabs, { Tab } from "@/components/ScrollTabs";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const paths: Tab[] = [
-    { url: `/my/lists`, label: "Your lists" },
-    { url: `/my/lists/saved`, label: "Saved lists" },
-  ];
-
   return (
     <Main>
       <Contents>
-        <Row className={"w-full justify-evenly"}>
-          <Col
-            className={
-              "mx-0 w-full max-w-[728px] md:mx-[24px] md:mb-[48px] md:mt-[52px]"
-            }
-          >
-            <Col className={"flex flex-col-reverse md:flex-col"}>
-              <Text
-                className={
-                  "ellipsisLine1 heading1 mb-[30px] mt-[24px] min-h-[30px] font-medium text-black md:my-0 md:min-h-[52px] md:text-[42px]"
-                }
-              >
-                {"Your library"}
-              </Text>
-            </Col>
-            <Col>
-              <ScrollTabs tabs={paths} />
-              {/*<TabContent target={subject} data={data?.main} />*/}
-            </Col>
-          </Col>
-          {children}
-        </Row>
+        <Row className={"w-full justify-evenly"}>{children}</Row>
       </Contents>
     </Main>
   );
--- src/app/[locale]/(auth)/my/lists/page.tsx
+++ src/app/[locale]/(auth)/my/lists/page.tsx
@@ -1,7 +1,56 @@
 import React from "react";
+import ScrollTabs, { Tab } from "@/components/ScrollTabs";
+import Col from "@/components/Layout/Col";
+import Text from "@/components/Layout/Text";
+import NavContentCSR from "@/app/[locale]/(auth)/my/lists/_component/NavContentCSR";
+import TabContentCSR from "@/app/[locale]/(auth)/my/lists/_component/TabContentCSR";
+import Row from "@/components/Layout/Row";
+import { Button } from "@/components/ui/button";
 
-function Page() {
-  return <div></div>;
+export default function Page() {
+  const paths: Tab[] = [
+    { url: `/my/lists`, label: "Your lists" },
+    { url: `/my/lists/saved`, label: "Saved lists" },
+  ];
+  return (
+    <>
+      <Col
+        className={
+          "mx-0 w-full max-w-[728px] md:mx-[24px] md:mb-[48px] md:mt-[52px]"
+        }
+      >
+        <Row className={"items-center justify-between"}>
+          <Text
+            className={
+              "ellipsisLine1 heading1 mb-[30px] mt-[24px] min-h-[30px] font-medium text-black md:my-0 md:min-h-[52px] md:text-[42px]"
+            }
+          >
+            {"Your library"}
+          </Text>
+          <Button
+            font={"body3"}
+            className={"h-[42px] px-[20px] py-2"}
+            rounded={"full"}
+          >
+            New list
+          </Button>
+        </Row>
+        <Col>
+          <ScrollTabs tabs={paths} />
+          <TabContentCSR />
+        </Col>
+      </Col>
+      <Col
+        className={
+          "hidden h-[100dvh] w-full max-w-[368px] border-l border-gray-200 lg:flex"
+        }
+        style={{
+          paddingLeft: "clamp(24px,24px + 100vw - 1080px,40px)",
+          paddingRight: "24px",
+        }}
+      >
+        <NavContentCSR />
+      </Col>
+    </>
+  );
 }
-
-export default Page;
